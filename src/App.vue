f865e92f8c3d7c8280355d696cbc6269792145ca
Ievgenii Izrailtenko <i.izrailtenko@mailbox.org>
2025-04-10 17:37:09 +0200

    Deliting AboutView from router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import HeaderView from './views/HeaderView.vue'
+import FooterView from './views/FooterView.vue'
 </script>
 
 <template>
-  <RouterView />
+  <div class="flex flex-col min-h-screen bg-amber-100">
+    <HeaderView class="bg-green-700 text-white text-shadow-amber-950 text-outline h-16" />
+    <RouterView class="flex-grow" />
+    <FooterView class="bg-amber-800 text-black h-24" />
+  </div>
 </template>
---
+++ src/views/HeaderView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <div>MyHeader</div>
+    <div class="text-red-400">Another Text</div>
+  </div>
+</template>
